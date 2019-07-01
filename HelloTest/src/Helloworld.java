7747e951062a323cf0c67b09ecf0b7c416f4bb73
chuzhigao <39247569+chuzhigao@users.noreply.github.com>
2019-07-01 12:27:17 +0800

    Update Helloworld.java

--- HelloTest/src/Helloworld.java
+++ HelloTest/src/Helloworld.java
@@ -2,6 +2,9 @@
 public class Helloworld {
   public static void main(String[] args) {
 		System.out.println("hello zhangsan ");
+	  	System.out.println("hello zhangsan ");
+	  
+	  System.out.println("hello zhangsan ");
 		
 		
 	}
